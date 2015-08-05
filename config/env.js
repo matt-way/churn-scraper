4cb46c5cf841acccfe5d0c3c27594429c49f1c67
Matt Way <matt__way@hotmail.com>
2015-08-05 21:49:34 +1000

    port fix

--- config/env.js
+++ config/env.js
@@ -5,7 +5,7 @@ var e = process.env;
 
 module.exports = {
 	accessKey: e.CHURN_SCRAPER_ACCESS_KEY,
-	port: e.CHURN_SCRAPER_PORT || 3000,
+	port: e.PORT || 3000,
 	api: e.CHURN_SCRAPER_API || 'http://api.churn.tv/',
 	DB: {
 		location: e.CHURN_SCRAPER_DB_LOCATION || 'mongodb://localhost:27017',
