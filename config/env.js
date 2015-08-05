b6077f0d44daf0173d0993c57b4f69253ca206b7
Matt Way <matt__way@hotmail.com>
2015-08-05 21:24:21 +1000

    db env update

--- config/env.js
+++ config/env.js
@@ -8,6 +8,8 @@ module.exports = {
 	port: e.CHURN_SCRAPER_PORT || 3000,
 	api: e.CHURN_SCRAPER_API || 'http://api.churn.tv/',
 	DB: {
-		location: e.CHURN_SCRAPER_DB_LOCATION || 'mongodb://localhost:27017'
+		location: e.CHURN_SCRAPER_DB_LOCATION || 'mongodb://localhost:27017',
+		user: e.CHURN_SCRAPER_DB_USER,
+		pass: e.CHURN_SCRAPER_DB_PASS
 	}
 };
